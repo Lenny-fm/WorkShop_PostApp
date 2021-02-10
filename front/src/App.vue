93209d0631eb195b8b8d887f008f5447c077473c
FRANCOIS-MARIE <Lenny@localhost.localdomain>
2021-02-10 12:35:02 +0400

    push

---
+++ front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderBar />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar.vue';
+export default {
+    components: {
+        HeaderBar,
+    },
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap');
+  *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+</style>
---
+++ front/src/components/HeaderBar.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <!-- HEADER -->
+        <header class="header">
+                <h1 class="header-logo">PostApp</h1>
+                    <h2>Home</h2>
+                    <h2>Login</h2>
+                    <h2>Sign up !</h2>
+        </header>
+    <div class="test">
+        <div class="container">
+            <h1 class="HeaderLogin">User Login</h1>
+            <input type="text" placeholder="Username" name="uname" required>
+            <input type="password" placeholder="Password" name="psw" required>
+            <button type="submit">Login</button>
+        </div>
+    </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        color:white;
+        float: right;
+        background-color: #45AAF2;
+        width: 100%;
+        margin-left: 0;
+        margin-right: 0;
+        height: 120px;
+        margin-top: -10px;
+        position: relative;
+    }
+    h2 {
+        font-size:xx-large;
+        margin-left: 40px;
+        float: right;
+        font-family: 'Roboto', sans-serif;
+        font-size: 25px;
+        margin-top: 2.8rem;
+        margin-right: 20px;
+        color: white;
+    }
+    .header-logo {
+        position: absolute;
+        top: 2.5rem;
+        left: 1rem;
+        font-family:'Lobster', cursive;
+    }
+    input {
+        padding: 12px 20px;
+        margin: 20px 0;
+        display: inline-block;
+        border:0px;
+        box-sizing: border-box;
+        width: 100%;
+        text-align: center;
+    }
+    .test {
+        height: 27rem;
+        width: 100%;
+        size: 20%;
+        position: relative;
+        .container {
+            position: absolute;
+            background-color: #45AAF2;
+            top: 80%;
+            left:50%;
+            transform: translate(-50%, -50%);
+        }
+    }
+    h1 {
+        font-family:'Lobster', cursive;
+        color: white;
+        text-align: center;
+    }
+</style>
